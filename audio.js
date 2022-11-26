10eee207b4af74d30c3f2aec0175c48bc9f5626a
David Eller <davidmatoseller@gmail.com>
2022-11-26 19:53:09 -0300

    menu :)

--- audio.js
+++ audio.js
@@ -1,11 +1,12 @@
-var audioGetCalled = false;
-function myFunction() {
-    if (audioGetCalled === false) {
+var audioPode = false;
+
+function comecaAudio() {
+    if (audioPode === false) {
         var musica = "MusicMenu.wav";
         var audio = new Audio(musica);
         audio.controls = false;
         document.body.appendChild(audio);
         audio.play();
-        audioGetCalled = true;
+        audioPode = true;
     }
 };
