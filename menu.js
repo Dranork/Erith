9fe7fcfd32e7a57719195107ed313c5f74788ccf
David Eller <davidmatoseller@gmail.com>
2022-11-26 19:27:39 -0300

    menu ;)

--- menu.js
+++ menu.js
@@ -1,5 +1,11 @@
-var musica = "MusicMenu.wav";
-var audio = new Audio(musica);
-audio.controls = false;
-document.body.appendChild(audio);
-audio.play();
+var audioGetCalled = false;
+function myFunction() {
+    if (audioGetCalled === false) {
+        var musica = "MusicMenu.wav";
+        var audio = new Audio(musica);
+        audio.controls = false;
+        document.body.appendChild(audio);
+        audio.play();
+        audioGetCalled = true;
+    }
+};
