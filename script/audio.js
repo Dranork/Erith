61002ff932edcbb0b3cb147ce063f4695bd0feb9
David Eller <davidmatoseller@gmail.com>
2022-11-27 13:01:16 -0300

    menu ;)

--- script/audio.js
+++ script/audio.js
@@ -2,7 +2,7 @@ var audioPode = false;
 
 function comecaAudio() {
     if (audioPode === false) {
-        var musica = "MusicMenu.wav";
+        var musica = "MusicMenu.mp3";
         var audio = new Audio(musica);
         audio.controls = false;
         audio.setAttribute("id","audio");
--- script/inicia.js
+++ script/inicia.js
@@ -1,7 +1,24 @@
 function inicio() {
-    var txt = ['Se tudo estiver certo, isso deve aparecer.', '', '', '', ''];
-    var speed = 50
-    var i = 0
+    var txt = ['Há muito tempo atrás...', 'Em uma pequena terra isolada, batizada de <div class="Erith">Erith<div> ','<div class="pecado">Suja de sangue, dor e ódio.<div>', '<div class="pecado">Manchada pelos pecados daqueles que à habitavam.<div>', 'Mesmo diante tanta desgraça,', 'Ainda havia esperança...', 'Ainda havia quem buscasse sua <div id="redencao">Redenção<div>'];
+    var nDialogo = 0;
+
+    function dialogos() {
+        if (txt.length != nDialogo) {
+            var falas = document.createElement("p");
+            falas.setAttribute("id", "dialogo");
+            document.body.appendChild(falas);
+            falas.innerHTML = txt[nDialogo];
+            setTimeout(function apagaFala() {     
+                falas.style.animation = "desaparecerTexto 3s";
+                falas.style.color = "black";
+                nDialogo++;
+                setTimeout(function () {    
+                    falas.remove();
+                    dialogos();
+                }, 3000);
+            }, 6000);
+        }  
+    };
 
 
     document.getElementById("audio").remove();
@@ -10,14 +27,5 @@ function inicio() {
     bodyStyle.backgroundImage = "none";
     bodyStyle.backgroundColor = "black";
     element.innerHTML = "";
-    setInterval.setTimeout(dialogos(), 2000)
-
-    function dialogos() {
-        var falas = document.createElement("p");
-        falas.setAttribute("id", "dialogo");
-        document.body.appendChild(falas);
-
-    };
-        
-
+    setTimeout(dialogos(), 2000);
 };
