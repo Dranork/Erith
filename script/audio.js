3d43fbb2a94399f39c65bd89768fb847e8627ac9
olsorriso123 <119892869+olsorriso123@users.noreply.github.com>
2022-12-05 19:14:16 -0300

    limpezinha de código
    arquivos principal.js e falas.js unidos em um só
    sistema de sidescroll atualizado pra não precisar mais de um div separado
    função de dialogos ajeitada

--- script/audio.js
+++ script/audio.js
@@ -1,22 +1,18 @@
 let audioPode = false;
 let configSalva = { 
     alturaSom : 100
+
 };
 let janelaAberta = false;
 
 function comecaAudio() {
     if (audioPode === false) {
-        if (localStorage.getItem('config') != null) {
-            let config = localStorage.getItem('config');
-            configSalva = JSON.parse(config);
-        }
         let musica = "MusicMenu.mp3";
         let audio = new Audio(musica);
         audio.controls = false;
         audio.setAttribute("id","audio");
         document.body.appendChild(audio);
         audio.play();
-        audio.volume = configSalva.alturaSom / 100
         audioPode = true;
     }
 };
@@ -37,21 +33,13 @@ function options() {
         aplicar.setAttribute("id","aplicar");
         aplicar.innerHTML = "Aplicar alterações";
         janela.appendChild(aplicar);
-        aplicar.addEventListener('click', salvarVolume);
-        
-        function salvarVolume() {
-            configSalva.alturaSom = volumeSlide.value;
-            let dados = JSON.stringify(configSalva);
-            localStorage.setItem('config', dados);
-            document.getElementById("audio").volume = configSalva.alturaSom / 100;
-        };
     }
 };
 
 function criarJanela() {
     let janela = document.createElement("div");
     janela.setAttribute("id", "janelinha");
-    document.getElementById("htmlAlteravel").appendChild(janela);
+    document.body.appendChild(janela);
     return janela;
 };
 
@@ -59,30 +47,3 @@ function sobreNos() {
     let janela = criarJanela();
 };
 
-window.onload = function chuvinha() {
-    function success(position) {
-        const lat  = position.coords.latitude;
-        const lon = position.coords.longitude;
-  
-        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=5033cbde8fac0b1b4f9e39292c1ffb0a`)
-          .then(response => response.json())
-          .then(data => {
-            switch (Math.trunc(data.weather[0].id / 100)) {
-                case 2:
-                    document.body.style.backgroundImage = "url('style/backgroundchuvendinho.gif')";
-                break;             
-                case 3:
-                    document.body.style.backgroundImage = "url('style/backgroundchuvendinho.gif')";
-                break;
-                case 5:
-                    document.body.style.backgroundImage = "url('style/backgroundchuvendinho.gif')";
-                break;
-            }
-          })
-      }
-    if (!navigator.geolocation) {
-        window.alert('Geolocation is not supported by your browser');
-    } else {
-        navigator.geolocation.getCurrentPosition(success);
-    }
-};
